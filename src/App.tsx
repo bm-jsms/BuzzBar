0693def9096fe13767a0132501bd3cf63d159fba
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-07-01 18:47:35 -0500

    ✨ [add] ScrollTrigger & SplitText - [add] Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import { gsap } from 'gsap';
+
+import { Navbar } from './components';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<main>
+			<Navbar />
+		</main>
+	);
 };
 
 export default App;
