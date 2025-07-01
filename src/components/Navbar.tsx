440592b20f11474502c4609e27f29cd7f38b27c7
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-07-01 18:46:06 -0500

    🎉 [create] Navbar - component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+	useGSAP(() => {
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: 'nav',
+				start: 'bottom top',
+			},
+		});
+
+		navTween.fromTo(
+			'nav',
+			{
+				backgroundColor: 'transparent',
+			},
+			{
+				backgroundColor: '#00000050',
+				backgroundFilter: 'blur(10px)',
+				duration: 1,
+				ease: 'power2.out',
+			},
+		);
+	}, []);
+	return (
+		<nav>
+			<div>
+				<a href='#home' className='flex items-center gap-2'>
+					<p>BuzzBar</p>
+					<img src='/images/logo.png' alt='logo' width='50' height='50' />
+				</a>
+				<ul>
+					{navLinks.map((link) => (
+						<li key={link.id}>
+							<a href={link.title}>{link.title}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export { Navbar };
