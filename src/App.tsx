e08129ae3590afac790663d02a803ba5ec1df58d
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-07-02 17:14:57 -0500

    ✨ [add] Art section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import { gsap } from 'gsap';
 
-import { About, Cocktails, Hero, Navbar } from './components';
+import { About, Art, Cocktails, Hero, Navbar } from './components';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +12,7 @@ const App = () => {
 			<Hero />
 			<Cocktails />
 			<About />
+			<Art />
 		</main>
 	);
 };
