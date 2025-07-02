9da764079aabc27d31310583df433c0501a641f2
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-07-02 18:29:44 -0500

    🎉 [create] AOS Wrapper - AOS init & AOS div

---
+++ src/components/animations/AosWrapper.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export const AOSInit = () => {
+	useEffect(() => {
+		AOS.init({
+			once: false,
+			duration: 800,
+		});
+	}, []);
+
+	return null;
+};
+
+export const AOSDiv = ({
+	children,
+	...props
+}: React.HTMLAttributes<HTMLDivElement>) => <div {...props}>{children}</div>;
